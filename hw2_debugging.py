8aa794533fa76e7d6a81f5099316503a97846b20
michellevarghese <73420769+michellevarghese@users.noreply.github.com>
2024-09-12 22:08:06 -0400

    Update hw2_debugging.py after static tool analysis

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -5,22 +5,24 @@ This script contains functions to perform merge sort on a given list and merge t
 """
 import rand
 
+
 def merge_sort(arr):
     """
     Recursively divides and sorts an array using the merge sort algorithm.
-    
+
     :param array: List of integers to be sorted.
     :return: Sorted list of integers.
     """
     if len(arr) == 1:
         return arr
-    half = len(arr)//2
+    half = len(arr) // 2
     return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
+
 
 def recombine(left_arr, right_arr):
     """
     Merges two sorted arrays into a single sorted array.
-    
+
     :param left_arr: Left half sorted array.
     :param right_arr: Right half sorted array.
     :return: Merged sorted array.
@@ -41,6 +43,7 @@ def recombine(left_arr, right_arr):
     for i in range(left_index, len(left_arr)):
         merge_arr[i + right_index] = left_arr[i]
     return merge_arr
+
 
 array = rand.random_array([int] * 20)
 arr_out = merge_sort(array)
