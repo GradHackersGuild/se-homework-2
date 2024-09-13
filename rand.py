5809c6e01980e1c5d7e75625ca89b9b508be3792
michellevarghese <73420769+michellevarghese@users.noreply.github.com>
2024-09-12 22:01:49 -0400

    Updated rand.py error

--- rand.py
+++ rand.py
@@ -6,7 +6,8 @@ This module contains a function to create a list of random integers.
 
 import random
 
-def random_array(arr)-> list[int]:
+
+def random_array(arr) -> list[int]:
     """
     Fill the provided list with random integers between 1 and 20.
 
